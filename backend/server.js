52e304b26368105e490910ac12aa752c9c332d87
balankdharan <balankdharan@gmail.com>
2024-05-04 21:34:12 +0530

    sample

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,7 @@
 const express = require("express");
 const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello world!");
+});
 app.listen(5000, () => console.log(`Server running on port 5000`));

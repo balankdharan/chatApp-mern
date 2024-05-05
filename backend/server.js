44c8335b4141b0679c1eccea75fb0be3e32b8a08
balankdharan <balankdharan@gmail.com>
2024-05-05 20:37:14 +0530

    API

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,11 @@
-const express = require("express");
+import express from "express";
+import dotenv from "dotenv";
 const app = express();
+
+dotenv.config();
+const PORT = process.env.PORT || 8000;
 
 app.get("/", (req, res) => {
   res.send("Hello world!");
 });
-app.listen(5000, () => console.log(`Server running on port 5000`));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));

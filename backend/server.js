8bee4221fece44e58e39b366694b99de9d058dea
balankdharan <balankdharan@gmail.com>
2024-05-08 22:28:21 +0530

    sample

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 const app = express();
+import authRoutes from "./routes/authRoutes.js";
 
 dotenv.config();
 const PORT = process.env.PORT || 8000;
@@ -8,4 +9,6 @@ const PORT = process.env.PORT || 8000;
 app.get("/", (req, res) => {
   res.send("Hello world!");
 });
+
+app.use("/api/auth", authRoutes);
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));

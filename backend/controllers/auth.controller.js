99abf4dbe11db73e67cdd04832f2c759b2fcdbb2
balankdharan <balankdharan@gmail.com>
2024-05-10 20:30:11 +0530

    model and controller function without hash

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,13 +1,46 @@
+import User from "../models/user.model.js";
+
 export const login = (req, res) => {
   console.log("Login user");
 };
 export const logout = (req, res) => {
   console.log("Logout user");
 };
-export const signUp = (req, res) => {
+export const signUp = async (req, res) => {
   try {
     const { fullName, username, password, confirmPassword, gender } = req.body;
+    if (password !== confirmPassword) {
+      return res.status(400).json({ error: "Password Don't match" });
+    }
+
+    const user = await User.findOne({ username });
+    if (user) {
+      return res.status(400).json({ error: "Username already exists" });
+    }
+
+    //Hash password
+
+    const boyProfilePic = `https://avatar.iran.liara.run/public/boy/?username=${username}`;
+    const girlProfilePic = `https://avatar.iran.liara.run/public/girl/?username=${username}`;
+
+    const newUser = new User({
+      fullName,
+      username,
+      password,
+      gender,
+      profilePic: gender === "male" ? boyProfilePic : girlProfilePic,
+    });
+
+    await newUser.save();
+
+    res.status(201).json({
+      _id: newUser._id,
+      fullName: newUser.fullName,
+      username: newUser.username,
+      profilePic: newUser.profilePic,
+    });
   } catch (err) {
     console.log("Error: " + err);
+    res.status(500).json({ error: "server error" });
   }
 };
--- backend/server.js
+++ backend/server.js
@@ -7,11 +7,13 @@ import connectToMongoDB from "./db/connectToMongoDB.js";
 dotenv.config();
 const PORT = process.env.PORT || 5000;
 
-app.get("/", (req, res) => {
-  res.send("Hello world!");
-});
+app.use(express.json());
 
 app.use("/api/auth", authRoutes);
+
+// app.get("/", (req, res) => {
+//   res.send("Hello world!");
+// });
 app.listen(PORT, () => {
   connectToMongoDB();
   console.log(`Server running on port ${PORT}`);

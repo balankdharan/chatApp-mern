cdc9009fdf989ae4a8fbf212e87b1ee3fbac66b0
balankdharan <balankdharan@gmail.com>
2024-05-11 19:19:01 +0530

    jwt and API check with cookie

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,4 +1,6 @@
 import User from "../models/user.model.js";
+import bcrypt from "bcryptjs";
+import generateTokenAndSetCookie from "../utils/generateToken.js";
 
 export const login = (req, res) => {
   console.log("Login user");
@@ -20,25 +22,32 @@ export const signUp = async (req, res) => {
 
     //Hash password
 
+    const salt = await bcrypt.genSalt(10);
+    const hasPassword = await bcrypt.hash(password, salt);
+
     const boyProfilePic = `https://avatar.iran.liara.run/public/boy/?username=${username}`;
     const girlProfilePic = `https://avatar.iran.liara.run/public/girl/?username=${username}`;
 
     const newUser = new User({
       fullName,
       username,
-      password,
+      password: hasPassword,
       gender,
       profilePic: gender === "male" ? boyProfilePic : girlProfilePic,
     });
 
-    await newUser.save();
-
-    res.status(201).json({
-      _id: newUser._id,
-      fullName: newUser.fullName,
-      username: newUser.username,
-      profilePic: newUser.profilePic,
-    });
+    if (newUser) {
+      await newUser.save();
+      generateTokenAndSetCookie(newUser._id, res);
+      res.status(201).json({
+        _id: newUser._id,
+        fullName: newUser.fullName,
+        username: newUser.username,
+        profilePic: newUser.profilePic,
+      });
+    } else {
+      res.status(400).json({ error: "Invalid data" });
+    }
   } catch (err) {
     console.log("Error: " + err);
     res.status(500).json({ error: "server error" });
